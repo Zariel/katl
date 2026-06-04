fae73bc2a589c8582c81156fadcf8e1b13467a7f
Chris Bannister <c.bannister@gmail.com>
2026-06-04 18:42:52 +0100

    dev: add nix shell
    Add a flake-backed development shell for Katl build, VM, Go, and
    protobuf tooling. Enable direnv with a guard for hosts that do not have
    Nix installed and ignore direnv's local cache directory.

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Katl development shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+      shellFor =
+        pkgs:
+        pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            cacert
+            cpio
+            cryptsetup
+            curl
+            dnf5
+            dosfstools
+            e2fsprogs
+            git
+            go
+            jq
+            mkosi
+            openssl
+            protobuf
+            protoc-gen-go
+            qemu_kvm
+            rpm
+            squashfsTools
+            systemd
+            util-linux
+            xz
+            zstd
+          ];
+        };
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = shellFor pkgs;
+          vm = shellFor pkgs;
+        }
+      );
+    };
+}
