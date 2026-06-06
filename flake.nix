c6b79cfab7ca1dd72abaebfde375f9c428c7dea9
Chris Bannister <c.bannister@gmail.com>
2026-06-06 20:53:03 +0100

    devshell: provide mkosi host tools
    Add the host-side tools that the local mkosi and vmtest paths expect
    when running from the Nix development shell. findutils supplies xargs,
    the dnf wrapper exposes dnf5 under the dnf name mkosi probes for, and
    TMPDIR defaults to /tmp so mkosi does not fall back to a read-only
    /var/tmp in constrained environments.

--- flake.nix
+++ flake.nix
@@ -16,6 +16,17 @@
       pkgsFor = system: import nixpkgs { inherit system; };
       shellFor =
         pkgs:
+        let
+          dnfCompat = pkgs.writeShellScriptBin "dnf" ''
+            state_home="''${XDG_STATE_HOME:-}"
+            if [ -z "$state_home" ]; then
+              state_home="''${TMPDIR:-/tmp}/katl-xdg-state"
+            fi
+            logdir="$state_home/dnf5"
+            mkdir -p "$logdir"
+            exec ${pkgs.dnf5}/bin/dnf5 --setopt=logdir="$logdir" "$@"
+          '';
+        in
         pkgs.mkShell {
           packages = with pkgs; [
             bashInteractive
@@ -28,6 +39,7 @@
             dosfstools
             e2fsprogs
             erofs-utils
+            findutils
             gawk
             git
             go
@@ -47,9 +59,11 @@
             util-linux
             xz
             zstd
+            dnfCompat
           ];
 
           shellHook = ''
+            export TMPDIR="''${TMPDIR:-/tmp}"
             export KATL_OVMF_CODE="''${KATL_OVMF_CODE:-${pkgs.OVMFFull.fd}/FV/OVMF_CODE.fd}"
             export KATL_OVMF_VARS="''${KATL_OVMF_VARS:-${pkgs.OVMFFull.fd}/FV/OVMF_VARS.fd}"
             export KATL_VMTEST_QEMU="''${KATL_VMTEST_QEMU:-${pkgs.qemu_kvm}/bin/qemu-system-x86_64}"
